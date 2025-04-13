f30398f57ac4da0d0c34aaf24561f12b010b07c5
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-04-12 21:49:18 -0700

    update: data

--- scripts.js
+++ scripts.js
@@ -1,97 +1,134 @@
-/**
- * Data Catalog Project Starter Code - SEA Stage 2
- *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
- */
-
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
-
-// This function adds cards the page to display the data in the array
-function showCards() {
-  const cardContainer = document.getElementById("card-container");
-  cardContainer.innerHTML = "";
-  const templateCard = document.querySelector(".card");
-
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
-
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
-
-    const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
-    cardContainer.appendChild(nextCard); // Add new card to the container
+const spells = [
+  {
+    id: 1, 
+    name: "Aguamenti",
+    category: ["Charm", "Conjuration"],
+    image: "https://static.wikia.nocookie.net/harrypotter/images/9/95/B6C28_Aguamenti_PM.png/revision/latest?cb=20241130182413",
+    pronunciation: "AH-gwah-MEN-tee",
+    description: "Produces a clean, drinkable jet of water from the wand tip.",
+    mentioned: [
+      "Used by Fleur Delacour in 1994 to extinguish her skirt, which had caught flame during a fight against a dragon.",
+      "Harry used this spell twice in 1997, both on the same night; once to attempt to provide a drink for Dumbledore.",
+      "Then again to help douse Hagrid's hut after it was set aflame by Thorfinn Rowle, who used the Fire-Making Spell."
+    ]
+  },
+  {
+    id: 2,
+    name: "Alarte Ascendare",
+    category: ["Charm"],
+    image: "https://static.wikia.nocookie.net/harrypotter/images/c/c4/Alarte_Ascendare.gif/revision/latest?cb=20190402152136",
+    pronunciation: "a-LAR-tay a-SEN-der-ay",
+    description: "Shoots the target high into the air.",
+    mentioned: [
+      "Used by Gilderoy Lockhart in 1992 to send a snake high into the air during the first and last meeting of the Duelling Club."
+    ]
+  },
+  {
+    id: 3,
+    name: "Anti-Intruder Jinx",
+    category: ["Jinx"],
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdUZd96O51hqa5yAvVljuqZ91orNzfoWb-4g&s",
+    pronunciation: "N/A",
+    description: "Prevents intruders from entering an area; similar in effect to the Anti-Apparition Charm.",
+    mentioned: [
+      "Placed on Hogwarts School of Witchcraft and Wizardry in 1996 for extra protection during an invasion of Death Eaters."
+    ]
+  },
+  {
+    id: 4,
+    name: "Antonin Dolohov's Curse",
+    category: ["Curse"],
+    image: "https://static.wikia.nocookie.net/harrypotter/images/6/69/DeathEatersBattle.jpg/revision/latest?cb=20081115070554",
+    pronunciation: "N/A",
+    description: "An unknown curse that causes injuries capable of killing if cast with sufficient power.",
+    mentioned: [
+      "Used by Antonin Dolohov during the Battle of the Department of Mysteries twice; while under a Silencing Charm, the curse's effect was weakened—saving Hermione from death but incapacitating her, requiring ten potions a day for recovery."
+    ]
+  },
+  {
+    id: 5,
+    name: "Aparecium",
+    category: ["Charm"],
+    image: "https://static.wikia.nocookie.net/harrypotter/images/0/02/Aparecium.jpg/revision/latest?cb=20180801200919",
+    pronunciation: "AH-par-EE-see-um",
+    description: "Reveals secret messages written in invisible ink or other hidden markings; also works against Concealing charms.",
+    mentioned: [
+      "Used (to no avail) in 1993 by Hermione Granger to try and reveal hidden writing in a diary."
+    ]
+  },
+  {
+    id: 6,
+    name: "Appare Vestigium",
+    category: ["Charm"],
+    image: "https://static.wikia.nocookie.net/harrypotter/images/0/01/COG_Newt_Appare_Vestigium.gif/revision/latest?cb=20190224141035",
+    pronunciation: "ah-PAR-ay ves-TEE-jee-um",
+    description: "Reveals traces of magic, including footprints and other track marks.",
+    mentioned: [
+      "Newton Scamander used it to search for traces of Porpentina Goldstein."
+    ]
+  },
+  {
+    id: 7,
+    name: "Avifors",
+    category: ["Transfiguration"],
+    image: "https://static.wikia.nocookie.net/harrypotter/images/9/96/Avifors.jpg/revision/latest?cb=20090904212154",
+    pronunciation: "AH-vi-fors",
+    description: "Transforms the target into a bird.",
+    mentioned: [
+      "Taught in Transfiguration class."
+    ]
+  },
+  {
+    id: 8,
+    name: "Avada Kedavra",
+    category: ["Curse"],
+    image: "https://static.wikia.nocookie.net/harrypotter/images/7/78/Avada-Kedavra.gif/revision/latest/thumbnail/width/360/height/360?cb=20180412031022",
+    pronunciation: "ah-VAH-dah keh-DAV-rah",
+    description: "Causes instantaneous and painless death, leaving no physical injury or trace of violence. Cast with a bright flash or a long stream of green light and a rushing noise.",
+    mentioned: [
+      "Used by Tom Riddle to murder many victims and sparingly on Harry Potter under unique circumstances."
+    ]
+  },
+  {
+    id: 9,
+    name: "Apparition",
+    category: ["Transportation"],
+    image: "https://static.wikia.nocookie.net/harrypotter/images/3/30/Hermione_and_Harry_apparating.gif/revision/latest/scale-to-width-down/250?cb=20240608194641",
+    pronunciation: "N/A",
+    description: "Magically teleports the user to another location instantly. The destination is one the user has seen or been to before and can sometimes transport multiple people if they hold on to each other (Side-Along Apparition). No incantation is required.",
+    mentioned: [
+      "Frequently used by wizards for rapid travel."
+    ]
+  },
+  {
+    id: 10,
+    name: "Anapneo",
+    category: ["Healing Spell", "Vanishment"],
+    image: "https://static.wikia.nocookie.net/harrypotter/images/2/2e/Celestina_Warbeck%27s_throat_unblocked_HM.png/revision/latest?cb=20220818132336",
+    pronunciation: "ah-NAP-nee-oh",
+    description: "Clears the target's airway if they are choking on something.",
+    mentioned: [
+      "Used by Horace Slughorn on Marcus Belby in 1996 when the latter choked on a pheasant."
+    ]
+  },
+  {
+    id: 11,
+    name: "Anteoculatia",
+    category: ["Dark Charm"],
+    image: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2024/11/pansy-parkinson-trying-to-turn-harry-to-voldemort-in-harry-potter-and-the-deathly-hallows.jpg",
+    pronunciation: "an-tee-oh-kyoo-LAY-chee-ah",
+    description: "Causes the target to grow antlers.",
+    mentioned: [
+      "Used to make Pansy Parkinson grow antlers in 1996."
+    ]
+  },
+  {
+    id: 12,
+    name: "Amato Animo Animato Animagus",
+    category: ["Transfiguration"],
+    image: "https://static.wikia.nocookie.net/harrypotter/images/8/8e/B3C18M1_Marauders.jpg/revision/latest?cb=20241207131613",
+    pronunciation: "ah-MAH-toh ah-NEE-moh ah-nee-MAH-toh an-a-MAY-jus",
+    description: "Spell used as part of the process of becoming an Animagus. The incantation is recited at sunrise and sundown, every day before consuming the Animagus Potion, and also just after a lightning storm starts while placing the wand’s tip over one's heart.",
+    mentioned: []
   }
-}
-
-function editCardContent(card, newTitle, newImageURL) {
-  card.style.display = "block";
-
-  const cardHeader = card.querySelector("h2");
-  cardHeader.textContent = newTitle;
-
-  const cardImage = card.querySelector("img");
-  cardImage.src = newImageURL;
-  cardImage.alt = newTitle + " Poster";
-
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
-}
-
-// This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
-
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!"
-  );
-}
-
-function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
-}
+];
