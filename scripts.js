70ff25e10cf3cd3c1a72dd0f1e240ef9ecafcef0
AureliaSindhu <aurelia.sindhu@gmail.com>
2025-04-15 13:24:26 -0700

    update: dataset

--- scripts.js
+++ scripts.js
@@ -1,137 +1,13 @@
-const spells = [
-  {
-    id: 1, 
-    name: "Aguamenti",
-    category: ["Charm", "Conjuration"],
-    image: "./img/aguamenti.webp",
-    pronunciation: "AH-gwah-MEN-tee",
-    description: "Produces a clean, drinkable jet of water from the wand tip.",
-    mentioned: [
-      "Used by Fleur Delacour in 1994 to extinguish her skirt, which had caught flame during a fight against a dragon.",
-      "Harry used this spell twice in 1997, both on the same night; once to attempt to provide a drink for Dumbledore.",
-      "Then again to help douse Hagrid's hut after it was set aflame by Thorfinn Rowle, who used the Fire-Making Spell."
-    ]
-  },
-  {
-    id: 2,
-    name: "Alarte Ascendare",
-    category: ["Charm"],
-    image: "./img/alarte.webp",
-    pronunciation: "a-LAR-tay a-SEN-der-ay",
-    description: "Shoots the target high into the air.",
-    mentioned: [
-      "Used by Gilderoy Lockhart in 1992 to send a snake high into the air during the first and last meeting of the Duelling Club."
-    ]
-  },
-  {
-    id: 3,
-    name: "Anti-Intruder Jinx",
-    category: ["Jinx"],
-    image: "./img/anti-intruder.webp",
-    pronunciation: "N/A",
-    description: "Prevents intruders from entering an area; similar in effect to the Anti-Apparition Charm.",
-    mentioned: [
-      "Placed on Hogwarts School of Witchcraft and Wizardry in 1996 for extra protection during an invasion of Death Eaters."
-    ]
-  },
-  {
-    id: 4,
-    name: "Antonin Dolohov's Curse",
-    category: ["Curse"],
-    image: "./img/antonin.webp",
-    pronunciation: "N/A",
-    description: "An unknown curse that causes injuries capable of killing if cast with sufficient power.",
-    mentioned: [
-      "Used by Antonin Dolohov during the Battle of the Department of Mysteries twice; while under a Silencing Charm, the curse's effect was weakened—saving Hermione from death but incapacitating her, requiring ten potions a day for recovery."
-    ]
-  },
-  {
-    id: 5,
-    name: "Aparecium",
-    category: ["Charm"],
-    image: "./img/aparecium.webp",
-    pronunciation: "AH-par-EE-see-um",
-    description: "Reveals secret messages written in invisible ink or other hidden markings; also works against Concealing charms.",
-    mentioned: [
-      "Used (to no avail) in 1993 by Hermione Granger to try and reveal hidden writing in a diary."
-    ]
-  },
-  {
-    id: 6,
-    name: "Appare Vestigium",
-    category: ["Charm"],
-    image: "./img/appare.webp",
-    pronunciation: "ah-PAR-ay ves-TEE-jee-um",
-    description: "Reveals traces of magic, including footprints and other track marks.",
-    mentioned: [
-      "Newton Scamander used it to search for traces of Porpentina Goldstein."
-    ]
-  },
-  {
-    id: 7,
-    name: "Avifors",
-    category: ["Transfiguration"],
-    image: "./img/avifors.webp",
-    pronunciation: "AH-vi-fors",
-    description: "Transforms the target into a bird.",
-    mentioned: [
-      "Taught in Transfiguration class."
-    ]
-  },
-  {
-    id: 8,
-    name: "Avada Kedavra",
-    category: ["Curse"],
-    image: "./img/avada.webp",
-    pronunciation: "ah-VAH-dah keh-DAV-rah",
-    description: "Causes instantaneous and painless death, leaving no physical injury or trace of violence. Cast with a bright flash or a long stream of green light and a rushing noise.",
-    mentioned: [
-      "Used by Tom Riddle to murder many victims and sparingly on Harry Potter under unique circumstances."
-    ]
-  },
-  {
-    id: 9,
-    name: "Apparition",
-    category: ["Transportation"],
-    image: "./img/apparition.webp",
-    pronunciation: "N/A",
-    description: "Magically teleports the user to another location instantly. The destination is one the user has seen or been to before and can sometimes transport multiple people if they hold on to each other (Side-Along Apparition). No incantation is required.",
-    mentioned: [
-      "Frequently used by wizards for rapid travel."
-    ]
-  },
-  {
-    id: 10,
-    name: "Anapneo",
-    category: ["Healing Spell", "Vanishment"],
-    image: "./img/anapneo.webp",
-    pronunciation: "ah-NAP-nee-oh",
-    description: "Clears the target's airway if they are choking on something.",
-    mentioned: [
-      "Used by Horace Slughorn on Marcus Belby in 1996 when the latter choked on a pheasant."
-    ]
-  },
-  {
-    id: 11,
-    name: "Anteoculatia",
-    category: ["Dark Charm"],
-    image: "./img/anteoculatia.avif",
-    pronunciation: "an-tee-oh-kyoo-LAY-chee-ah",
-    description: "Causes the target to grow antlers.",
-    mentioned: [
-      "Used to make Pansy Parkinson grow antlers in 1996."
-    ]
-  },
-  {
-    id: 12,
-    name: "Amato Animo Animato Animagus",
-    category: ["Transfiguration"],
-    image: "./img/amato.avif",
-    pronunciation: "ah-MAH-toh ah-NEE-moh ah-nee-MAH-toh an-a-MAY-jus",
-    description: "Spell used as part of the process of becoming an Animagus. The incantation is recited at sunrise and sundown, every day before consuming the Animagus Potion, and also just after a lightning storm starts while placing the wand’s tip over one's heart.",
-    mentioned: []
-  }
-];
+let spells =[]
+
+fetch("spells.json")
+  .then((response) => response.json())
+  .then((data) => {
+    spells = data;
+    displaySpells(spells);
+    populateFilterOptions();
+  })
+  .catch((error) => console.error("Error loading spells:", error));
 
 // ------------------
 // functions 
@@ -232,9 +108,7 @@ function filterSpells(){
 
   const filteredSpells = spells.filter(spell => {
     const matchesSearch = spell.name.toLowerCase().includes(searchTerm) || spell.description.toLowerCase().includes(searchTerm);
-    const matchesCategory = (filterValue === "all") || spell.category.some(
-      cat => cat.toLowerCase() === filterValue
-    );
+    const matchesCategory = (filterValue === "all") || spell.category.some(cat => cat.toLowerCase() === filterValue);
     return matchesSearch && matchesCategory;
   });
   displaySpells(filteredSpells);
